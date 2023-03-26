6195ddda8173f25f6a2f218394dc3d339e005d75
Lalit Singh <lalit@192.168.1.8>
2023-03-27 00:14:15 +0530

    update terraform for azure

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-lalit-azure"
   os_type             = "Linux"
 
   container {
@@ -30,3 +30,51 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+
+resource "azurerm_storage_account" "udacity" {
+  name                     = "udacity-lalit"
+  resource_group_name      = azurerm_resource_group.udacity.name
+  location                 = azurerm_resource_group.udacity.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "udacity" {
+  name                         = "udacity-lalit-azure-sql"
+  resource_group_name          = azurerm_resource_group.udacity.name
+  location                     = azurerm_resource_group.udacity.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator12345"
+  administrator_login_password = "thisIsDog1112345"
+
+  tags = {
+    environment = "Dev"
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "udacity" {
+  name     = "udacity-lalit-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "udacity" {
+  name                = "udacity-lalit_service-plan"
+  resource_group_name = azurerm_resource_group.udacity.name
+  location            = azurerm_resource_group.udacity.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "udacity" {
+  name                = "udacity-lalit-azure-dotnet-app"
+  resource_group_name = azurerm_resource_group.udacity.name
+  location            = azurerm_service_plan.udacity.location
+  service_plan_id     = azurerm_service_plan.udacity.id
+
+  site_config {}
+}
