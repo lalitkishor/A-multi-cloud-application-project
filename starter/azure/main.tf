3f2fd3e96d84b49d589584608a9c855b886ce809
Lalit Singh <lalit@192.168.1.8>
2023-03-27 08:09:02 +0530

    update reference data block

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -33,16 +33,16 @@ resource "azurerm_container_group" "udacity" {
 
 resource "azurerm_storage_account" "udacity" {
   name                     = "udacity-lalit"
-  resource_group_name      = azurerm_resource_group.udacity.name
-  location                 = azurerm_resource_group.udacity.location
+  resource_group_name      = data.azurerm_resource_group.udacity.name
+  location                 = data.azurerm_resource_group.udacity.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_sql_server" "udacity" {
   name                         = "udacity-lalit-azure-sql"
-  resource_group_name          = azurerm_resource_group.udacity.name
-  location                     = azurerm_resource_group.udacity.location
+  resource_group_name          = data.azurerm_resource_group.udacity.name
+  location                     = data.azurerm_resource_group.udacity.location
   version                      = "12.0"
   administrator_login          = "mradministrator12345"
   administrator_login_password = "thisIsDog1112345"
@@ -64,16 +64,16 @@ resource "azurerm_resource_group" "udacity" {
 
 resource "azurerm_service_plan" "udacity" {
   name                = "udacity-lalit_service-plan"
-  resource_group_name = azurerm_resource_group.udacity.name
-  location            = azurerm_resource_group.udacity.location
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_web_app" "udacity" {
   name                = "udacity-lalit-azure-dotnet-app"
-  resource_group_name = azurerm_resource_group.udacity.name
-  location            = azurerm_service_plan.udacity.location
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_service_plan.udacity.location
   service_plan_id     = azurerm_service_plan.udacity.id
 
   site_config {}
